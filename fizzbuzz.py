065e7dc6eb46c2063ad20787b8a70e4845eeeac0
Yusuke5011 <79350753+Yusuke5011@users.noreply.github.com>
2022-05-27 08:08:28 +0900

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,15 @@
+for num in range(1, 101):
+    string = ''
+
+    # ここから記述
+    if num % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(i) 
+    # ここまで記述
+
+    print(string)
