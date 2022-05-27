e5c5d77eeacb8501bdd59a69fbb393cc63a36ea3
Yusuke5011 <79350753+Yusuke5011@users.noreply.github.com>
2022-05-27 09:06:49 +0900

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,14 +2,20 @@ for num in range(1, 101):
     string = ''
 
     # ここから記述
-    if num % 3 == 0 and i % 5 == 0:
+    
+    if num % 3 == 0 and i % 5 == 0:　
+    # numが３の倍数かつ５の倍数（15の倍数）の時
         print("FizzBuzz")
     elif num % 3 == 0:
+    # numが15の倍数ではなく3の倍数の時
         print("Fizz")
     elif num % 5 == 0:
+    # numが15の倍数でも、３の倍数でもなく、5の倍数の時
         print("Buzz")
     else:
+    # それ以外の時
         print(i) 
+        
     # ここまで記述
 
     print(string)
